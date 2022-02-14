c3b1a5b41e3d246eeb4e388228f71b784c9b826c
Agnes Kis <agi.uqbar@gmail.com>
2022-02-14 15:20:45 +0100

    :sparkles: downloading assets action, editorcfg

---
+++ actions/download-private-asset/README.md
@@ -0,0 +1,34 @@
+# Install private repo asset with PAT (Personal Access Token)
+
+## Usage
+
+```yaml
+    - uses: gameanalytics/github/actions/install-private-asset@v1
+      with:
+        repo: orgname/reponame
+        version: 1.0.0
+        asset: assetname-1.0.0-...whl
+      env:
+        # Your personal access token which
+        #  accesses the private asset
+        token: ${{ secrets.TOKEN }}
+```
+
+Or
+
+```yaml
+    - uses: gameanalytics/github/actions/install-private-asset@v1
+      with:
+        repo: orgname/reponame
+        version: latest
+        asset: projectname-latest.zip
+      env:
+        # Your personal access token which
+        #  accesses the private asset
+        token: ${{ secrets.TOKEN }}
+```
+
+## Credits
+
+* https://gist.github.com/maxim/6e15aa45ba010ab030c4
+* https://github.com/wyozi/download-gh-release-asset
---
+++ actions/download-private-asset/download-private-asset.sh
@@ -0,0 +1,43 @@
+#!/bin/sh -l
+
+REPO=$1
+VERSION=$2
+FILE=$3
+
+GITHUB="https://api.github.com"
+
+# Ensure that the PAT is included in the env
+if [[ -z "$TOKEN" ]]; then
+  echo "Set the TOKEN (PAT) env variable."
+  exit 1
+fi
+
+echo "Start downloading $FILE from $REPO (version: $VERSION)"
+
+function gh_curl() {
+  curl -H "Authorization: token $TOKEN" \
+       -H "Accept: application/vnd.github.v3.raw" \
+       $@
+}
+
+if [ "$VERSION" = "latest" ]; then
+  # Github should return the latest release first.
+  parser=".[0].assets | map(select(.name == \"$FILE\"))[0].id"
+else
+  parser=". | map(select(.tag_name == \"$VERSION\"))[0].assets | map(select(.name == \"$FILE\"))[0].id"
+fi;
+
+asset_id=`gh_curl -s $GITHUB/repos/$REPO/releases | jq "$parser"`
+
+if [ "$asset_id" = "null" ]; then
+  echo "ERROR: version not found $VERSION"
+  exit 1
+fi;
+
+wget -q --auth-no-challenge --header='Accept:application/octet-stream' \
+  https://$TOKEN:@api.github.com/repos/$REPO/releases/assets/$asset_id \
+  -O $FILE
+
+# Output
+location="`pwd`/$FILE"
+echo ::set-output name=location::$location
